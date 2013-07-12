5fe357f0154b5634a6b02fc73d53d505437ba02e
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-07-12 09:26:57 +0200

    Restructuring and adding ext_emconf.php to get this template installed

---
+++ ext_emconf.php
@@ -0,0 +1,45 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tmpl_adw".
+ *
+ * Auto generated 12-07-2013 09:25
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'ADW Template',
+	'description' => 'Template for ADW Website',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Ingo Pfennigstorf',
+	'author_email' => 'pfennigstorf@sub.uni-goettingen.de',
+	'author_company' => 'www.sub.uni-goettingen.de',
+	'constraints' =>
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.7.0-6.1.99',
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
