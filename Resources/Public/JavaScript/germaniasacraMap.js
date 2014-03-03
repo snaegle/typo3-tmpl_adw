94c7f8ea7a2dc00aa3f2b676400fd76a0d67251e
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-03 14:33:03 +0100

    Modified Output of coordinates for leaflet in website ADWD-900

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -1,9 +1,12 @@
 // Generates a Map using leafletJs
 
-function initMap() {
+function leafletInitMap() {
   var map = L.map('lmap').setView([51.32, 9.56], 5);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   }).addTo(map);
+}
+function leafletMapAddMarker (isso) {
+	alert("nix");
 }
 
