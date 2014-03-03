af574a30e7eb1b5b683a1751195c62f29b979c45
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-03 10:59:12 +0100

    refactor use leaflet instead of geohash and changed css into scss ADWD-900

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -1,8 +1,9 @@
 // Generates a Map using leafletJs
 
 function initMap() {
-  var map = L.map('lMap').setView([51.32, 9.56], 7);
+  var map = L.map('lmap').setView([51.32, 9.56], 5);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   }).addTo(map);
 }
+
