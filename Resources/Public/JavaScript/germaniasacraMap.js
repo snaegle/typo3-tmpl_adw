27bd3566ae61ab72d484a9db898d819b04fd1a9e
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-06 09:20:40 +0100

    Modified Map shoes now markers in Africa and points to them ADWD-900

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -1,16 +1,11 @@
 // Generates a Map using leafletJs
 
 function leafletInitMap() {
-  var map = L.map('lmap').setView([51.32, 9.56], 5);
+  var map = L.map('lmap').setView([13.56, 51.32], 3);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
   }).addTo(map);
 	L.geoJson(leafletJsonCoords, {
 						style: function(feature) {return feature.properties;}
 					}).addTo(map);
-}
-function leafletAddToMap() {
-	L.geoJson(leafletJsonCoords, {
-					style: function(feature) {return feature.properties;}
-				}).addTo(map);
 }
