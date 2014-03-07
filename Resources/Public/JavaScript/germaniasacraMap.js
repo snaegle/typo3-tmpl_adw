336922fa87e8841f98a38dddeeaa12047c59dc02
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-07 10:55:44 +0100

    Modified Map shows markers in Germany ADWD-900

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -1,11 +1,11 @@
 // Generates a Map using leafletJs
 
 function leafletInitMap() {
-  var map = L.map('lmap').setView([13.56, 51.32], 3);
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
-  }).addTo(map);
-	L.geoJson(leafletJsonCoords, {
-						style: function(feature) {return feature.properties;}
-					}).addTo(map);
+  }).addTo(leafletMap);
+}
+
+function leafletIterate(lat,lng) {
+	L.marker([lat, lng]).addTo(leafletMap);
 }
