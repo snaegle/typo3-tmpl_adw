e719e992ba7247c1ef521434c427ace51300460c
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-03-13 09:57:15 +0100

    Updated realurl configuration for nice URLs in Germania Sacra (ADWD-967)

---
+++ Configuration/System/realurl_conf.php
@@ -0,0 +1,46 @@
+<?php
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array(
+		'dev.adw-goe.de' => array(
+				'init' => array(
+						'enableCHashCache' => true,
+						'appendMissingSlash' => 'ifNotFile,redirect',
+						'adminJumpToBackend' => true,
+						'enableUrlDecodeCache' => true,
+						'enableUrlEncodeCache' => true,
+						'emptyUrlReturnValue' => '/'
+				),
+				'pagePath' => array(
+						'type' => 'user',
+						'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+						'spaceCharacter' => '-',
+						'languageGetVar' => 'L',
+						'rootpage_id' => '1'
+				),
+				'fileName' => array(
+						'defaultToHTMLsuffixOnPrev' => 0,
+						'acceptHTMLsuffix' => 1,
+				),
+				'preVars' => array(
+						array(
+								'GETvar' => 'L',
+								'valueMap' => array(
+										'en' => 1,
+										'fr' => 2,
+										'it' => 3,
+										'es' => 4,
+										'cz' => 5),
+								'noMatch' => 'bypass'
+						),
+				),
+				'postVarSets' => array(
+						'_DEFAULT' => array(
+								'gsn' => array(
+										array(
+												'GETvar' => 'tx_find_find[id]',
+										)
+								),
+						)
+				)
+		),
+);
+?>
