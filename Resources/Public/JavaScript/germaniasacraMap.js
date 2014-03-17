cfbcecc65ab790f2a938cc3d625ec62381b940d9
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-17 12:40:38 +0100

    In Resources/Public/Css/adw.css Pfad zu fancybox_sprite.png geaendert

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -3,8 +3,15 @@
 function leafletInitMap() {
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(leafletMap);
 	leafletMap.scrollWheelZoom.disable();
+
+   $("a#bigMap").fancybox({
+	   "autoDimensions": false,
+	   "width": 350,
+	   "height": "auto"
+                          });
 }
 
 function leafletIterate(lat,lng) {
 	L.marker([lat, lng]).addTo(leafletMap);
 }
+
