d55fb694e8ba25f2bf930a83798c127a8e27ced0
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-17 14:43:17 +0100

    task link "Karte gross anzeigen" entfernt (ADWD-1018)

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -4,11 +4,13 @@ function leafletInitMap() {
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(leafletMap);
 	leafletMap.scrollWheelZoom.disable();
 
-   $("a#bigMap").fancybox({
-	   "autoDimensions": false,
-	   "width": 350,
-	   "height": "auto"
+   $("a.bigMap").fancybox({
+	   "padding": 10,
+	                          afterClose: function() {
+		                          $("#lmap").show();
+	                          }
                           });
+
 }
 
 function leafletIterate(lat,lng) {
