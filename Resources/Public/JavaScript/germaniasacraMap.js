222979fde8dca25c6e38bedd66e9a4589a96b9b4
Sibylle Naegle <naegle@sub.uni-goettingen.de>
2014-03-17 09:37:10 +0100

    modified map, now without scrollWheelZoom (ADWD-994)

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -2,6 +2,7 @@
 
 function leafletInitMap() {
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(leafletMap);
+	leafletMap.scrollWheelZoom.disable();
 }
 
 function leafletIterate(lat,lng) {
