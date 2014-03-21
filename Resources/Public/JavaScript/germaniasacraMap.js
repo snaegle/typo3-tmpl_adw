3e53dc9f08e459e51cc7702ed13da1ebae9acaa2
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-03-21 10:19:03 +0100

    [TASK] Structural refactorings
    Removed all css files from Resources/Private/Templates/Extensions/...
    and inserted them into the main structure.
    Adopted the TypoScript to ommit the missing CSS files.
    Merged all tx-find css into one scss file and put it under _Includes
    in the main Scss directory.

--- Resources/Public/JavaScript/germaniasacraMap.js
+++ Resources/Public/JavaScript/germaniasacraMap.js
@@ -1,19 +1,16 @@
 // Generates a Map using leafletJs
-
 function leafletInitMap() {
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png').addTo(leafletMap);
 	leafletMap.scrollWheelZoom.disable();
 
    $("a.leaflet-big-map").fancybox({
-	   "padding": 10,
-	                          afterClose: function() {
-		                          $("#leaflet-map").show();
-	                          }
-                          });
-
+		"padding": 10,
+		afterClose: function() {
+			$("#leaflet-map").show();
+		}
+	});
 }
 
 function leafletIterate(lat,lng) {
 	L.marker([lat, lng]).addTo(leafletMap);
 }
-
