7d60dd2397fe68f7c6f03b50e4872e0a1cea1f7c
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-04-29 17:49:19 +0200

    [TASK] ADWD-946 First show people with date ranges

---
+++ Classes/ViewHelpers/SortEntriesByDateViewHelper.php
@@ -0,0 +1,56 @@
+<?php
+namespace Subugoe\TmplAdw\ViewHelpers;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Ingo Pfennigstorf <pfennigstorf@sub-goettingen.de>
+ *      Goettingen State Library
+ *  
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Description 
+ */
+class SortEntriesByDateViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @param array $array
+	 * @param string $as
+	 * @return mixed
+	 */
+	public function render($array, $as) {
+		uasort($array, function($a, $b) {
+
+			if ($a['von_verbal'] !== '') {
+				return -1;
+			} else {
+				return 1;
+			}
+
+		});
+
+		$this->templateVariableContainer->add($as, $array);
+		$output = $this->renderChildren();
+		$this->templateVariableContainer->remove($as);
+
+		return $output;
+	}
+}
