3805908310d41bd333e9195906e5d6ca541545ed
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2014-07-02 14:50:17 +0200

    [TASK] Update reST Settings

--- Documentation/_make/conf.py
+++ Documentation/_make/conf.py
@@ -89,7 +89,7 @@ exclude_trees = ['_make']
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#show_authors = False
+show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -169,10 +169,10 @@ html_static_path = ['../_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
